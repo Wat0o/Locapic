88c531a6e0d6553ffed09e6cfb334a93500d51dd
Benjamin Prot <prot.benj@gmail.com>
2025-05-12 16:53:59 +0200

    ajout .env

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://protbenj:kiL6FdhQql1cKVlx@myfirstdatabase.xlsj8vw.mongodb.net/locapic'
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
